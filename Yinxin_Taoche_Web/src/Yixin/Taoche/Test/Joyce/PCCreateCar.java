3df96b717a7250aa0415fbe042539b09597318b2
fuxiaobai <fuxiaobai@FUXB-33.yixin.dk>
2017-07-17 10:29:24 +0800

    修改引用文件

--- Yinxin_Taoche_Web/src/Yixin/Taoche/Test/Joyce/PCCreateCar.java
+++ Yinxin_Taoche_Web/src/Yixin/Taoche/Test/Joyce/PCCreateCar.java
@@ -1,11 +1,11 @@
 package Yixin.Taoche.Test.Joyce;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
