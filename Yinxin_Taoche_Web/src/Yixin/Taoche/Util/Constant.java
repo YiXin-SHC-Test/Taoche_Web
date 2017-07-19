c2e8e1e868951c9e26d24afac10104198f703825
magl <823358533@qq.com>
2017-07-19 22:28:36 +0800

    常用变量信息

---
+++ Yinxin_Taoche_Web/src/Yixin/Taoche/Util/Constant.java
@@ -0,0 +1,8 @@
+package Yixin.Taoche.Util;
+
+public class Constant {
+	// 定义测试网址的常量
+	public static final String TaoChe_Url = "http://www.taoche.com";
+	// 定义测试手机号常量
+	public static final String PhoneNumber = "13000000000";
+}
